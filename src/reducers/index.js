379941349338bb08336feba8740e07536da503f6
VictorParodi <victor.parodi.montero@gmail.com>
2019-05-18 10:19:34 -0500

    app1 commit 1: building reducers, action creators, and using Provider and createStore

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers }  from 'redux';
+
+const songsReducer = () => {
+    return [
+        {title: 'Song 1', duration: '1:20'},
+        {title: 'Song 2', duration: '2:20'},
+        {title: 'Song 3', duration: '3:20'},
+        {title: 'Song 4', duration: '4:20'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SELECT_SONG') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
+
+export default combineReducers({
+    song: songsReducer,
+    selectedSong: selectedSongReducer
+});
